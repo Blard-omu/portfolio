af431abaefe47b4e99353fb009241118f674cf26
Blard <peteromu76@gmail.com>
2023-11-30 18:28:35 +0100

    webkit css

--- src/components/header/Cta.jsx
+++ src/components/header/Cta.jsx
@@ -4,7 +4,7 @@ import CV from '../../assets/CV.pdf'
 const Cta = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn btn-primary'>Download CV</a>
+        <a href={CV} download className='btn btn-primary me-3'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Chat</a>
 
     </div>
