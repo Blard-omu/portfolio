0327c990767b8a0f120432df29168e26a3aa9935
Blard <peteromu76@gmail.com>
2023-11-30 18:12:39 +0100

    webkit css

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,57 +1,56 @@
 import React from 'react'
 import './experience.css'
-import {BsFillPatchCheckFill} from 'react-icons/bs'
+import { ImHtmlFive2 } from "react-icons/im";
+import { IoLogoCss3, IoLogoNodejs } from "react-icons/io5";
+import { IoLogoJavascript } from "react-icons/io";
+import { FaBootstrap, FaPython } from "react-icons/fa";
+import { SiTailwindcss, SiDjango, SiExpress, SiMysql, SiMongodb, SiPostgresql, SiGraphql, SiSwagger } from "react-icons/si";
+import { GrReactjs } from "react-icons/gr";
 
 const Experience = () => {
   return (
     <section id='experience'>
-      <h5>The Skills I Have</h5>
-      <h2>My Experience</h2>
+      <h5>Here's</h5>
+      <h2>My Stacks</h2>
 
       <div className="container experience_container">
         <div className="experience_frontend">
           <h3>Frontend Development</h3>
           <div className="experience_content">
             <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon'/>
-              <div>
-              <h4>HTML</h4>
-              <small className='text-light'>Experienced</small>
+              <div className='text-center'>
+              <h4><ImHtmlFive2/></h4>
+              <small className='text-light'>HTML</small>
               </div>
             </article>
             <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon'/>
-              <div>
-              <h4>CSS</h4>
-              <small className='text-light'>Intermediate</small>
+              <div className='text-center'>
+              <h4><IoLogoCss3/></h4>
+              <small className='text-light'>CSS</small>
               </div>
             </article>
             <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon'/>
-              <div>
-              <h4>JavaScript</h4>
-              <small className='text-light'>Experienced</small>
+              <div className='text-center'>
+              <h4><FaBootstrap/></h4>
+              <small className='text-light'>Bootstrap</small>
               </div>
             </article>
             <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon'/>
-              <div>
-              <h4>Bootstrap</h4>
-              <small className='text-light'>Experienced</small>
+              <div className='text-center'>
+              <h4><SiTailwindcss/></h4>
+              <small className='text-light'>Tailwind</small>
               </div>
             </article>
             <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon'/>
-              <div>
-              <h4>Python</h4>
-              <small className='text-light'>Experienced</small>
+              <div className='text-center'>
+              <h4><IoLogoJavascript/></h4>
+              <small className='text-light'>Javascript</small>
               </div>
             </article>
             <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon'/>
-              <div>
-              <h4>React</h4>
-              <small className='text-light'>Experienced</small>
+              <div className='text-center'>
+              <h4><GrReactjs/></h4>
+              <small className='text-light'>React</small>
               </div>
             </article>
           </div>
@@ -60,43 +59,61 @@ const Experience = () => {
         <div className="experience_backend">
           <h3>Backend Development</h3>
           <div className="experience_content">
-            <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon'/>
-              <div>
-              <h4>Django</h4>
-              <small className='text-light'>Experienced</small>
+          <article className='experience_details'>
+              <div className='text-center'>
+              <h4><FaPython/></h4>
+              <small className='text-light'>Python</small>
               </div>
             </article>
             <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon'/>
-              <div>
-              <h4>Node JS</h4>
-              <small className='text-light'>Intermediate</small>
+              <div className='text-center'>
+              <h4><SiDjango/></h4>
+              <small className='text-light'>Django</small>
               </div>
             </article>
             <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon'/>
-              <div>
-              <h4>MongoDB</h4>
-              <small className='text-light'>Basic</small>
+              <div className='text-center'>
+              <h4><IoLogoNodejs/></h4>
+              <small className='text-light'>Node</small>
               </div>
             </article>
             <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon'/>
-              <div>
-              <h4>MySQL</h4>
-              <small className='text-light'>Intermediate</small>
+              <div className='text-center'>
+              <h4><SiExpress/></h4>
+              <small className='text-light'>Express</small>
               </div>
             </article>
             <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon'/>
-              <div>
-              <h4>Python</h4>
-              <small className='text-light'>Experienced</small>
+              <div className='text-center'>
+              <h4><SiMysql/></h4>
+              <small className='text-light'>MySQL</small>
               </div>
             </article>
+            <article className='experience_details'>
+              <div className='text-center'>
+              <h4><SiMongodb/></h4>
+              <small className='text-light'>MongoDB</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <div className='text-center'>
+              <h4><SiPostgresql/></h4>
+              <small className='text-light'>PostgreSQL</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <div className='text-center'>
+              <h4><SiGraphql/></h4>
+              <small className='text-light'>GraphQL</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <div className='text-center'>
+              <h4><SiSwagger/></h4>
+              <small className='text-light'>Swagger-UI</small>
+              </div>
+            </article>           
           </div>
-
         </div>
       </div>
     </section>
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,120 +1,160 @@
-import React from 'react'
-import './services.css'
-import {BiCheck} from 'react-icons/bi'
+import React from "react";
+import "./services.css";
+import { BiCheck } from "react-icons/bi";
 
 const Services = () => {
   return (
-    <section id='services'>
+    <section id="services">
       <h5>What I Offer</h5>
       <h2>Services</h2>
 
       <div className="container services_container">
         <article className="service">
           <div className="service_head">
-            <h3>Data Analysis</h3>
-          </div>
-
-          <ul className='service_list'>
-            <li>
-              <BiCheck className='service_list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </li>
-            <li>
-              <BiCheck className='service_list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </li>
-            <li>
-              <BiCheck className='service_list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </li>
-            <li>
-              <BiCheck className='service_list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </li>
-            <li>
-              <BiCheck className='service_list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </li>
-            <li>
-              <BiCheck className='service_list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </li>
-          </ul>
-        </article>
-       {/* end of data analysis */}
-       <article className="service">
-          <div className="service_head">
             <h3>Web Development</h3>
           </div>
 
-          <ul className='service_list'>
+          <ul className="service_list">
             <li>
-              <BiCheck className='service_list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <span>
+                <BiCheck className="service_list-icon" />
+              </span>
+              <p>
+                Expertise in building fast end-to-end web applications using
+                MERN stack
+              </p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <span>
+                <BiCheck className="service_list-icon" />
+              </span>
+              <p>
+                Design and implementation of secure and scalable RESTful APIs.
+              </p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <span>
+                <BiCheck className="service_list-icon" />
+              </span>
+              <p>Building of responsive web applications</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <span>
+                <BiCheck className="service_list-icon" />
+              </span>
+              <p>Debugging</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <span>
+                <BiCheck className="service_list-icon" />
+              </span>
+              <p>Deployments</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <span>
+                <BiCheck className="service_list-icon" />
+              </span>
+              <p>Performance check</p>
             </li>
-            {/* <li>
-              <BiCheck className='service_list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </li> */}
           </ul>
         </article>
         {/* end of web development */}
         <article className="service">
           <div className="service_head">
-            <h3>Data Structures</h3>
+            <h3>Training</h3>
           </div>
 
-          <ul className='service_list'>
+          <ul className="service_list">
             <li>
-              <BiCheck className='service_list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <span>
+                <BiCheck className="service_list-icon" />
+              </span>
+              <p>Fullstack development, covering MERN, Python, Django.</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <span>
+                <BiCheck className="service_list-icon" />
+              </span>
+              <p>Offering guardiance and mentorship to aspiring developers</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <span>
+                <BiCheck className="service_list-icon" />
+              </span>
+              <p>Physical and Online Coaching</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
+              <BiCheck className="service_list-icon" />
+              <p>Conducting workshops on web development</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <span>
+                <BiCheck className="service_list-icon" />
+              </span>
+              <p>Technical Curricula Development</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <span>
+                <BiCheck className="service_list-icon" />
+              </span>
+              <p>Product Research</p>
             </li>
           </ul>
         </article>
-        {/* end of data structures */}
+        {/* end of web development */}
+        <article className="service">
+          <div className="service_head">
+            <h3>Technical Writing</h3>
+          </div>
+
+          <ul className="service_list">
+            <li>
+              <span>
+                <BiCheck className="service_list-icon" />
+              </span>
+              <p>
+                Creating of learner friendly Github repositories covering core
+                concepts in Javascript, Python, Node, etc.
+              </p>
+            </li>
+            <li>
+              <span>
+                <BiCheck className="service_list-icon" />
+              </span>
+              <p>
+                Developing step-by-step tutorials on various web development
+                concepts, tools, and frameworks for learners and professionals.
+              </p>
+            </li>
+            <li>
+              <span>
+                <BiCheck className="service_list-icon" />
+              </span>
+              <p>
+                Providing clear and comprehensive documentation for codebases,
+                APIs, and technical projects to enhance understanding and
+                maintainability.
+              </p>
+            </li>
+            <li>
+              <span>
+                <BiCheck className="service_list-icon" />
+              </span>
+              <p>Open source contributions</p>
+            </li>
+            {/* <li>
+              <BiCheck className='service_list-icon'/>
+              <p>Blogs</p>
+            </li>
+            <li>
+              <BiCheck className='service_list-icon'/>
+              <p>Workshops</p>
+            </li> */}
+          </ul>
+        </article>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
