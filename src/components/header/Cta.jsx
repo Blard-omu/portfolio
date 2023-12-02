b5b7932a1eab50d428401a5edcc1a67b2afd1839
Blard <peteromu76@gmail.com>
2023-12-02 01:41:09 +0100

    small devices done

--- src/components/header/Cta.jsx
+++ src/components/header/Cta.jsx
@@ -4,7 +4,7 @@ import CV from '../../assets/CV.pdf'
 const Cta = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn btn-primary me-3'>Download CV</a>
+        <a href={CV} download className='btn btn-primary me-1 px-2 me-md-3 '>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Chat</a>
 
     </div>
